40cfa067cad9dca1ca99220ee4f55da3ae079767
MarijnS95 <marijns95@gmail.com>
2019-09-14 22:05:15 +0200

    HACKS FOR MAINLINE.

--- common-treble.mk
+++ common-treble.mk
@@ -28,7 +28,7 @@ else
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl
+    android.hardware.graphics.mapper@2.0-impl-2.1
 
 endif
 
@@ -58,83 +58,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.2-impl
 
-# Camera
-ifeq ($(TARGET_USES_64BIT_CAMERA),true)
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl:64 \
-    android.hardware.camera.provider@2.4-service_64
-else
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl:32 \
-    android.hardware.camera.provider@2.4-service
-endif
-
-# Wi-Fi
-PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service
-
 # NFC packages
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service
-
-# GNSS
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.1-impl-qti \
-    android.hardware.gnss@2.1-service-qti
-
-# Light
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.sony
-
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service.sony
-
-# Sensors
-# hardware.ssc.so links against display mappers, of which
-# the interface libraries are explicitly included here:
-PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
-    vendor.qti.hardware.display.mapper@1.1.vendor \
-    vendor.qti.hardware.display.mapper@3.0.vendor
-
-ifeq ($(TARGET_VIBRATOR_V1_2),true)
-# QTI Haptics Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator@1.2-service
-else
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
-endif
-
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.sony
-
-# Gatekeeper passthrough service
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-service
-
-ifeq ($(TARGET_LEGACY_KEYMASTER),true)
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
-# Gatekeeper libhardware module passthrough
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl
-else ifeq ($(TARGET_KEYMASTER_V4),true)
-# Keymaster 4 passthrough service init file
-# (executable is on odm)
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0-service-qti.rc
-else
-# Keymaster 3 passthrough service
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-service
-endif
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -151,11 +77,6 @@ else
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 endif
-
-# Thermal HAL
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
 
 # Power
 PRODUCT_PACKAGES += \
