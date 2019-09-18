51d0037a76263eadce96db83505c7f8fbad813e9
MarijnS95 <marijns95@gmail.com>
2019-09-18 10:45:08 +0200

    [LINUX-MAINLINE] Delete packages incompatible with mainline headers.

--- common-packages.mk
+++ common-packages.mk
@@ -12,45 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TEMPORARY: These libraries are deprecated, not referenced by any AOSP
-# nor OSS HAL, but are still required by odm blobs (short term).
-# We don't add a dependency on the vndk variants as those end up in
-# /system but require these in /vendor instead:
-PRODUCT_PACKAGES += \
-    libhwbinder.vendor \
-    libhidltransport.vendor
-
-# Audio
-PRODUCT_PACKAGES += \
-    audio.a2dp.default \
-    audio.bluetooth.default \
-    audio.hearing_aid.default \
-    audio.r_submix.default \
-    audio.usb.default \
-    libaudio-resampler
-
-# Audio - A2DP offloading
-PRODUCT_PACKAGES += \
-    liba2dpoffload \
-    libbthost_if
-
-# For audio.primary
-PRODUCT_PACKAGES += \
-    cplay \
-    libaudioroute \
-    libtinyalsa \
-    tinycap \
-    tinymix \
-    tinypcminfo \
-    tinyplay
-
-# Audio effects
-PRODUCT_PACKAGES += \
-    libqcompostprocbundle \
-    libqcomvisualizer \
-    libqcomvoiceprocessing \
-    libvolumelistener
-
 # TODO Local package in crosshatch \
     libqcomvoiceprocessingdescriptors \
 
@@ -64,69 +25,10 @@ PRODUCT_PACKAGES += \
     libqdutils \
     libqdMetaData
 
-# OMX
-PRODUCT_PACKAGES += \
-    libc2dcolorconvert \
-    libstagefrighthw \
-    libOmxCore \
-    libmm-omxcore \
-    libOmxVdec \
-    libOmxVdecHevc \
-    libOmxVenc
-
-# GPS
-PRODUCT_PACKAGES += \
-    libloc_core \
-    libgps.utils \
-    liblocation_api \
-    libloc_pla \
-    libbatching \
-    libgeofencing \
-    libgnss
-
-# Location
-PRODUCT_PACKAGES += \
-    libloc_api_v02 \
-    libloc_ds_api \
-    libgnsspps \
-    libsynergy_loc_api
-
-# IMS (OSS)
-PRODUCT_PACKAGES += \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext \
-    ims-ext-common \
-    ims_ext_common.xml
-
-# IPA
-PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect.vendor \
-    IPACM_cfg.xml \
-    ipacm
-
-# WLAN
-PRODUCT_PACKAGES += \
-    hostapd \
-    libwpa_client \
-    wpa_supplicant \
-    wpa_supplicant.conf \
-    libwpa_client
-
 # NFC packages
 PRODUCT_PACKAGES += \
     NfcNci \
     Tag
-
-# CAMERA
-PRODUCT_PACKAGES += \
-    libmmcamera_interface \
-    libmmjpeg_interface \
-    libmmlib2d_interface \
-    libmm-qcamera \
-    libqomx_core
 
 # OSS Time service
 PRODUCT_PACKAGES += \
@@ -140,22 +42,6 @@ PRODUCT_PACKAGES += \
 # OSS WIFI and BT MAC tool
 PRODUCT_PACKAGES += \
     macaddrsetup
-
-# Carrier configuration
-PRODUCT_PACKAGES += \
-    CarrierConfig
-
-# Telephony
-PRODUCT_PACKAGES += \
-    HookMediator \
-    ImsHelper \
-    ModemConfig \
-    QcRilAm \
-    SimDetect
-
-# QCOM Data
-PRODUCT_PACKAGES += \
-    librmnetctl
 
 # RIL
 PRODUCT_PACKAGES += \
