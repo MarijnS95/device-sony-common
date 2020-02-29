93120f9f18233b9b97bbeec52c5e61bb2d7bfa68
Marijn Suijten <marijns95@gmail.com>
2020-02-29 21:47:53 +0100

    common: Forcibly include customization.mk at the beginning of common.mk
    This resolves two subtle issues with the inherit-product mechanism:
    - Inherited files are evaluated later, at least after _common.mk_ has
      been fully read despite the call to inherit-product being at the top
      of the file;
    - The ordering between other inherit-product calls is not guaranteed;
      the list is sorted to remove duplicates and provide consistency.
    
    The observed problem at hand is overriding TARGET_USE_QCRILD in
    customization.mk, as is happening in PE. Due to sorting common-init.mk
    (which uses this variable) is evaluated _before_ customization.mk,
    leading to the wrong rc files ending up on vendor.img.
    CommonConfig.mk on the other hand is parsed after the entire product
    inheritance tree is evaluated, hence the right vintf is selected
    according to this variable.
    
    This issue is fixed by forcefully including customization.mk at the
    "right" (or at the very least expected) step in the build process.
    Repository maintainers are aware that this file should only be included
    once, which is safe enough to do away with the single-evaluation
    guarantees inherit-product provides.
    
    Source:
    https://android.googlesource.com/platform/build/+/android-10.0.0_r26/core/product.mk#364
    > # To be called from product makefiles, and is later evaluated during
    > # the import-nodes call below. It does three things:
    
    Signed-off-by: Marijn Suijten <marijns95@gmail.com>

--- common.mk
+++ common.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+-include device/sony/customization/customization.mk
+
 # Common path
 COMMON_PATH := device/sony/common
 
