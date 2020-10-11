8a0c999970a38472b49c4536e8a60d66c365a1b0
Marijn Suijten <marijns95@gmail.com>
2020-10-11 13:55:57 +0200

    Include mandatory health package

--- common-treble.mk
+++ common-treble.mk
@@ -18,6 +18,10 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-service \
+    android.hardware.health@2.0-impl
+
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.1-service
