1bb6279903403ac3f865115f3bf21791efbb8d18
Marijn Suijten <marijn.suijten@somainline.org>
2022-01-21 11:26:03 +0100

    packages: Build bootctl into system partition by default (development)

--- common-packages.mk
+++ common-packages.mk
@@ -202,3 +202,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     ODMVersionCheck
+
+# Include the bootctl binary for controlling android.hardware.boot on A/B devices
+PRODUCT_PACKAGES += \
+    bootctl
